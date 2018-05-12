f1ba11f2720a863f80a69b57841c0f28b427f4d3
anuroopa18 <tsanuroopa@gmail.com>
2018-05-12 17:27:34 -0400

    changes to user template"
    "

--- src/main/webapp/jquery/components/user-admin/user-admin.controller.client.js
+++ src/main/webapp/jquery/components/user-admin/user-admin.controller.client.js
@@ -1,10 +1,78 @@
 (function () {
+    var tbody;
+    var template;
+    var userService = new UserServiceClient();
     jQuery(main);
+
     function main() {
-        var h1 = jQuery('h1');
-        h1.css('color', 'red');
-        h1.html('User Administration');
+        tbody = $('tbody');
+        template = $('.template');
+        $('.wbdv-create').click(createUser);
+        findAllUsers();
     }
+
+    function findAllUsers() {
+        userService.findAllUsers().then(renderUsers);
+    }
+
+    function createUser() {
+        console.log('createUser');
+        var username = $('#usernameFld').val();
+        var password = $('#passwordFld').val();
+        var firstName = $('#firstNameFld').val();
+        var lastName = $('#lastNameFld').val();
+        var role = $('#roleFld').val();
+
+        var user = {
+            username: username,
+            password: password,
+            firstName: firstName,
+            lastName: lastName,
+            role: role
+
+        }
+        userService
+            .createUser(user)
+            .then(findAllUsers);
+
+    }
+
+    function renderUsers(users) {
+        tbody.empty();
+        for (var i = 0; i < users.length; i++) {
+            var user = users[i];
+            var clone = template.clone();
+            clone.attr('id', user.id)
+            clone.removeClass();
+            clone.find('.delete').click(deleteUser);
+            clone.find('.edit').click(editUser);
+            clone.find('.username')
+                .html(user.username);
+            clone.find('.firstName')
+                .html(user.firstName);
+            clone.find('.lastName')
+                .html(user.lastName);
+            clone.find('.role')
+                .html(user.role);
+            tbody.append(clone);
+        }
+
+        function deleteUser(event) {
+            console.log('deleteUser');
+            console.log(event);
+            var deletebtn = $(event.currentTarget);
+            var userId = deletebtn.parent().parent().parent().attr('id');
+            userService
+                .deleteUser(userId)
+                .then(findAllUsers);
+        }
+
+        function editUser(event) {
+            console.log('editUser');
+            console.log(event);
+        }
+    }
+
 })();
 
 
