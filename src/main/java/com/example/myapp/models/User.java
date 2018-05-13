51563a94e1fc8d0c882e71d333c937b003ced1ca
anuroopa18 <tsanuroopa@gmail.com>
2018-05-13 15:10:26 -0400

    User model done

--- src/main/java/com/example/myapp/models/User.java
+++ src/main/java/com/example/myapp/models/User.java
@@ -15,7 +15,29 @@ public class User {
 	private String firstName;
 	private String lastName;
 	private String role;
+	private String phone;
+	private String email;
+	private String dateOfBirth;
 	
+	
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
 	public String getRole() {
 		return role;
 	}
