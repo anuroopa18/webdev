c51a33c60dfc82eba6a0059d33125d75be63df12
anuroopa18 <tsanuroopa@gmail.com>
2018-05-14 01:43:54 -0400

    changed type of dob

--- src/main/java/webdev/models/User.java
+++ src/main/java/webdev/models/User.java
@@ -1,5 +1,7 @@
 package webdev.models;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,7 +19,7 @@ public class User {
 	private String role;
 	private String phone;
 	private String email;
-	private String dateOfBirth;
+	private Date dateOfBirth;
 	
 	
 	public String getPhone() {
@@ -32,10 +34,10 @@ public class User {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getDateOfBirth() {
+	public Date getDateOfBirth() {
 		return dateOfBirth;
 	}
-	public void setDateOfBirth(String dateOfBirth) {
+	public void setDateOfBirth(Date dateOfBirth) {
 		this.dateOfBirth = dateOfBirth;
 	}
 	public String getRole() {
--- src/main/java/webdev/models/user.sql
+++ src/main/java/webdev/models/user.sql
@@ -1,13 +1,13 @@
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date_of_birth` datetime DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
   `first_name` varchar(255) DEFAULT NULL,
   `last_name` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  `role` varchar(255) DEFAULT NULL,
-  `date_of_birth` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
   `phone` varchar(255) DEFAULT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=123 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 SELECT * FROM heroku_c28d73bdec2f39a.user;
