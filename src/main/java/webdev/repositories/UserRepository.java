c895624fa841c49385181fd0aaa9ac40d9aa4cad
anuroopa18 <tsanuroopa@gmail.com>
2018-05-13 22:13:40 -0400

    user repo modified for register

--- src/main/java/webdev/repositories/UserRepository.java
+++ src/main/java/webdev/repositories/UserRepository.java
@@ -1,9 +1,12 @@
 package webdev.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import webdev.models.User;
 
 public interface UserRepository extends CrudRepository<User, Integer>{
 
+	Optional<User> findUserByUsername(String username);
 }
