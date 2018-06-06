f0f99c9aa58aa45bb38d1b198f9a9dac5beefa74
anuroopa18 <tsanuroopa@gmail.com>
2018-06-06 19:58:44 -0400

    Exam service

--- src/main/java/webdev/services/ExamService.java
+++ src/main/java/webdev/services/ExamService.java
@@ -9,8 +9,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import webdev.models.Assignment;
 import webdev.models.Exam;
 import webdev.models.Lesson;
 import webdev.models.Widget;
@@ -84,6 +87,21 @@ public class ExamService {
 			return examRepository.save(newExam);
 		}
 		return null;		
+	}
+	
+	@PutMapping("/api/exam/{id}")
+	public Exam updateExam(@PathVariable("id") int eid, @RequestBody Exam newExam) {
+		Optional<Exam> data = examRepository.findById(eid);
+		if(data.isPresent()) {
+			Exam exam = data.get();
+			if(newExam.getExamTitle() != null && !newExam.getExamTitle().equals("") ) {
+				exam.setExamTitle(newExam.getExamTitle());
+			}
+			examRepository.save(exam);
+			return exam;
+			
+		}
+		return null;
 	}
 
 	
